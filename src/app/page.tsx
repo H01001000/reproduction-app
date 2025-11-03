75a350b2e47322c8e0bbc89cfd9d3a70c41772c4
H01001000 <heiheiho000@gmail.com>
2025-11-03 10:01:21 -0800

    remove non related stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import ImageWrapper from "./ImageWrapper";
+import Image from "next/image.js";
 
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
   return <>
-    <ImageWrapper
+    <Image
       src="https://upload.wikimedia.org/wikipedia/commons/1/11/Test-Logo.svg"
       fill
-      alt="The back of Jonathan Ho on the edge of Tai Mo Shan"
+      alt=""
     />
   </>;
 }
